81e770e6b5bdabae9366dbaba3781ce749c8a0a5
Dilka Prabhashi <Dilka Prabhashi@DESKTOP-N9TS8UN>
2022-09-17 15:07:32 +0530

    Add project files.

---
+++ DapperCurdApplication/Controllers/StudentsController.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperCurdApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public StudentsController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Students>>> GetAllStudents()
+        {
+
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<Students> students = await SelectAllStudents(connection);
+            return Ok(students);
+        }
+
+
+        [HttpGet("{StuId}")]
+        public async Task<ActionResult<List<Students>>> GetStudents(int StuId)
+        {
+
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var student = await connection.QueryFirstAsync<Students>("select * from StudentsTb where studentid=@StudentID",
+                new {StudentID= StuId });
+            return Ok(student);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Students>>> CreateStudents(Students student)
+        {
+
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into StudentsTb (firstname,lastname,address,mobile)values(@FirstName,@LastName,@Address,@Mobile)", student);
+            return Ok(await SelectAllStudents(connection));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Students>>> UpdateStudents(Students student)
+        {
+
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update  StudentsTb set firstname=@FirstName,lastname=@LastName,address=@Address,mobile=@Mobile where studentid=@StudentID", student);
+            return Ok(await SelectAllStudents(connection));
+        }
+
+        [HttpDelete("{StuId}")]
+        public async Task<ActionResult<List<Students>>> DeleteStudents(int StuId)
+        {
+
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("delete from  StudentsTb where studentid=@StudentID", new { StudentID = StuId });
+            return Ok(await SelectAllStudents(connection));
+        }
+
+        private static async Task<IEnumerable<Students>> SelectAllStudents(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Students>("select * from StudentsTb ");
+        }
+
+    }
+}
+
---
+++ DapperCurdApplication/Students.cs
@@ -0,0 +1,16 @@
+﻿namespace DapperCurdApplication
+{
+    public class Students
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string Mobile { get; set; } = string.Empty;
+
+
+    }
+}
